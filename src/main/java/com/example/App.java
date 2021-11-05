65e47eb40ec43c1c2ce08410710af9489223e7c9
Internetpatrullen <hk@internetpatrullen.se>
2021-11-05 17:29:39 +0100

    Major improv, much better

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Optional;
 
 
 /**
@@ -20,6 +22,26 @@ public class App
         return al.stream().mapToInt(line -> (line.split(" ")).length).sum();
 
     }
+    private static String longestWord(ArrayList<String> al)
+    {
+        ArrayList<String> words = new ArrayList<String>();
+
+        for (String s: al) {
+            words.addAll(Arrays.asList(s.split(" ")));
+        }
+        
+        return words
+        .stream()
+        .reduce(
+            (longest, next) -> {
+                if (longest.length() > next.length()) 
+                    return longest; 
+                else 
+                    return next;
+            })
+        .orElse("");
+
+    }
     public static void main( String[] args )
     {
         StringFetch sf = new StringFetch(new Scanner(System.in), "Enter text", "stop");
@@ -33,5 +55,9 @@ public class App
         int wordsum = sumWords(all_reads);
 
         System.out.println("You entered " + wordsum + " amount of words.");
+
+        String longest = longestWord(all_reads);
+
+        System.out.println("You entered " + longest + " amount of words.");
     }
 }
